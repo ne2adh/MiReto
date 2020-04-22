c5a653087edafd51fa29df821d1028deb99bcdb2
Giancarlo Delgadillo <giancarlodcwork@gmail.com>
2020-04-22 01:47:16 -0400

    quizdata fix

--- src/data/quizData.js
+++ src/data/quizData.js
@@ -1,26 +1,65 @@
 export const quizData = [
   {
     id: 0,
-    question: ` What is the Capital Of India ? Who is the CEO of Tesla Motors?`,
-    options: [`New Delhi`, `Mumbai`, `Kolkatta`],
-    answer: `New Delhi`
+    question: `Los únicos organelos presentes en las células procariotas son?`,
+    options: [`Nucleolo`, `Todas`, `Ribosomas`, `Usosomas`, `Ninguno`],
+    answer: `Ninguno`
   },
   {
     id: 1,
-    question: `Who is the CEO of Tesla Motors?`,
-    options: [`Bill Gates`, `Steve Jobs`, `Elon Musk`],
-    answer: `Elon Musk`
+    question: `Los Microtubulos, microfilamentos y filamentos intermedios conforman el?`,
+    options: [`Citoesqueleto`, `Todas`, `Aparato de Golgi`, `Ninguna`, `Ninguno`],
+    answer: `Citoesqueleto`
   },
   {
     id: 2,
-    question: `Name World's Richest Man?`,
-    options: [`Jeff Bezo`, `Bill Gates`, `Mark Zuckerberg`],
-    answer: `Jeff Bezo`
+    question: `En la organización jerárquica de los seres vivos, el orden que se sigue es?`,
+    options: [`Órgano, célula, tejido, población, biosfera, ecosistema`, 
+    `Todas`, `Célula, órgano, tejido, organismo, población, comunidad, biosfera`,`ninguna`,`Célula, tejido, órgano, organismo, población, comunidad, ecosistema`],
+    answer: `ninguna`
   },
   {
     id: 3,
-    question: `Name World's Richest Man?`,
-    options: [`Jeff Bezo`, `Bill Gates`, `Mark Zuckerberg`],
-    answer: `Jeff Bezo`
+    question: `El ultimo nivel de organización que existe en el planeta es?`,
+    options: [`La comunidad`, `El ecosistema`, `La biosfera`,`Todos`,`Ninguna`],
+    answer: `La biosfera`
+  },
+  {
+    id: 4,
+    question: `Cuál de las siguientes son características de los seres vivos?`,
+    options: [`Se reproducen`, `Responden a estímulos`, `Son complejos y están organizados`,`Todos`,`Ninguna`],
+    answer: `Todos`
+  },
+  {
+    id: 5,
+    question: `Los bioelementos primarios, que forman parte de un ser vivo son?`,
+	options: [`Oxigeno (O), Azufre (S), Hidrogeno (H)`, 
+	`Nitrógeno (N), Oxigeno (O), Calcio (NA)`, `carbono (C), hidrógeno (H), oxígeno (O) y nitrógeno (N)`,
+	`carbono (C), Ástato (At), Oxigeno (O), Calcio (NA)`,`Hidrogeno (H), Cloro (CL), Oxigeno (O)`],
+    answer: `carbono (C), hidrógeno (H), oxígeno (O) y nitrógeno (N)`
+  },
+  {
+    id: 6,
+    question: `Las especies vegetales poseen?`,
+    options: [`Pared Celular`, `Cloroplastos`, `Células eucariotas`,`Todas`,`Ninguna`],
+    answer: `Pared Celular`
+  },
+  {
+    id: 7,
+    question: `El sistema de nomenclatura binominal o binomial fue establecido por?`,
+    options: [`Linneo`, `Darwin`, `Lamark`,`Pasteur`,`Ninguno`],
+    answer: `Linneo`
+  },
+  {
+    id: 8,
+    question: `El sistema nomenclatura binomial está constituido fundamentalmente por?`,
+    options: [`Especie y género`, `Genero y especie`, `Genero, especies y familia`,`Subgénero y genero`,`Ninguno`],
+    answer: `Genero y especie`
+  },
+  {
+    id: 9,
+    question: `La parte de la tierra donde existe vida, que consiste en una película delgada que se desplaza sobre la superficie de la tierra se llama?`,
+    options: [`Ecología`, `Biosfera`, `Recurso natural`,`desarrollo sostenible`,`Ninguno`],
+    answer: `Biosfera`
   },
 ];
